e1cf22976a69386e414e68a0df1dfae867d3774c
hieuntcasso <94815095+hieuntcasso@users.noreply.github.com>
2022-03-30 16:27:55 +0700

    add header footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,15 +1,39 @@
+import logo from '../../assets/logo.png'
+import facebook from '../../assets/facebook.png'
+import instagram from '../../assets/instagram.png'
+import twitter from '../../assets/twitter.png'
 
 
-
-
-const Footer = ()=>{
+const Footer = () => {
 
 
     return (
-        <>
-
-        <h1>Footer</h1>
-        </>
+        <div className='fixed bottom-0 right-0 left-0 shadow-[10px_35px_60px_-15px_rgba(0,0,0,0.8)] p-[30px]'>
+            <div className=' grid grid-cols-3'>
+                <div className='col-span-1 flex flex-row items-center '>
+                    <img src={logo} alt="" className='rounded-full w-[90px] h-[90px] ' />
+                    <span className=' text-[#F7941E] text-[30px] font-bold'>E-CODE</span>
+                </div>
+                <div className='col-span-1 flex text-[#003663]'>
+                    <ul className='m-4'>
+                        <li className='font-bold m-4'>About Us</li>
+                        <li className='m-4'>Introduce</li>
+                        <li className='m-4'>Business Partner</li>
+                    </ul>
+                    <ul className='m-4'>
+                        <li className='font-bold m-4'>Help</li>
+                        <li className='m-4'>Contact Us</li>
+                        <li className='m-4'>Term</li>
+                    </ul>
+                </div>
+                <div className='col-span-1 flex mt-5'>
+                    <img src={facebook} alt="" className='rounded-full w-12 h-12 mx-3' />
+                    <img src={instagram} alt="" className='rounded-full w-12 h-12 mx-3' />
+                    <img src={twitter} alt="" className='rounded-full w-12 h-12 mx-3' />
+                </div>
+            </div>
+            <p>© 2022 Nike, Inc. All Rights Reserved</p>
+        </div>
     )
 }
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,30 @@
 
-
+import logo from '../../assets/logo.png'
+import avatar from '../../assets/avatar.png'
+import {Link } from 'react-router-dom'
 
 
 const Header = () => {
 
 
     return (
-        <div className="sticky top-0 left-0 right-0 border-solid border-2 border-indigo-600">
-            <h1>Header</h1>
+        <div className="sticky top-0 left-0 right-0  text-[#003663] font-bold grid grid-cols-4 px-8 shadow-md	">
+            <div className='col-span-1 flex'>
+                <img src={logo} alt="" className='rounded-full w-11 flex items-center' />
+                <span className='flex items-center text-[#F7941E] text-[20px]'>E-CODE</span>
+            </div>
+            <div className='col-span-2'>
+                <ul className='flex flex-row justify-around	items-center h-full'>
+                    <Link to="/">Home</Link>
+                    <Link to="/my-course">My Courses</Link>
+                    <Link to="/blog">Blog</Link>
+                    <Link to="">Contact Us</Link>
+                </ul>
+            </div>
+            <div className='col-span-1 flex flex-row items-center justify-end'>
+                <img src={avatar} alt="" className='rounded-full w-10 mr-2 p-[2px]' />
+                <span>Hieu Nguyen</span>
+            </div>
         </div>
     )
 }
